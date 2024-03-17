85c96aab4d84f0968c092fd89503334e162baf6c
oc-dave <135023771+oc-dave@users.noreply.github.com>
2024-03-17 21:41:15 +0100

    Update mailSender.js

--- task/mailSender.js
+++ task/mailSender.js
@@ -19,36 +19,3 @@ function senderEmail() {
 
 
 
-
-// Get the phone number input field
-const phoneNumberInput = document.getElementById('phone');
-
-// Add event listener to listen for changes in the input value
-phoneNumberInput.addEventListener('input', () => {
-  // Retrieve the entered phone number value
-  const phoneNumber = phoneNumberInput.value;
-
-  // Regular expressions to match phone number patterns
-  const mtnPattern = /^(\+234|0)803|806|810|813|814|816|903|906/;
-  const gloPattern = /^(\+234|0)805|807|811|815|905|0705|0805|0807|0811|0815|0905/;
-  const airtelPattern = /^(\+234|0)802|803|807|808|812|902|0701|0708|0802|0808|0812|0902/;
-  const etisalatPattern = /^(\+234|0)809|810|818|908|0818|0908/;
-
-  // Check the phone number against the patterns and update the UI
-  if (mtnPattern.test(phoneNumber)) {
-    // Display MTN logo
-    // You can update the UI by adding the MTN logo image dynamically
-  } else if (gloPattern.test(phoneNumber)) {
-    // Display Glo logo
-    // You can update the UI by adding the Glo logo image dynamically
-  } else if (airtelPattern.test(phoneNumber)) {
-    // Display Airtel logo
-    // You can update the UI by adding the Airtel logo image dynamically
-  } else if (etisalatPattern.test(phoneNumber)) {
-    // Display Etisalat logo
-    // You can update the UI by adding the Etisalat logo image dynamically
-  } else {
-    // No matching telecom provider found
-    // You can update the UI to remove any previously displayed logo
-  }
-});
